5ede21c37dfdade41b940b947f683949c5e6e74b
Sincerly <1475590636@qq.com>
2017-04-18 18:02:47 +0800

    新增初步Base

---
+++ app/src/main/java/com/sincerly/intimate/activity/TestActivity.java
@@ -0,0 +1,31 @@
+package com.sincerly.intimate.activity;
+
+import com.sincerly.intimate.R;
+import com.sincerly.intimate.base.BaseActivity;
+
+/**
+ * Created by Administrator on 2017/4/18.
+ */
+
+public class TestActivity extends BaseActivity {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_test;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initEvent() {
+
+    }
+
+    @Override
+    protected void setContentViewFront() {
+        super.setContentViewFront();
+    }
+
+}
---
+++ app/src/main/java/com/sincerly/intimate/base/BaseActivity.java
@@ -0,0 +1,59 @@
+package com.sincerly.intimate.base;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.sincerly.intimate.R;
+
+/**
+ * Created by Administrator on 2017/4/18.
+ */
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentViewFront();
+        setContentView(getLayoutId());
+    }
+
+    protected void setContentViewFront(){
+        //TODO:用来设置基础属性 例如无标题，沉浸式
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    protected abstract int getLayoutId();
+    protected abstract void initView();
+    protected abstract void initEvent();
+}
